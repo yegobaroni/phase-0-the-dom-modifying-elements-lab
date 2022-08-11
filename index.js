0850ec015399ab08c814a440ca741da6a3762e68
yego <yegokipkemoi254@gmail.com>
2022-08-11 23:32:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+/*const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.setAttribute('id','h1');
+newHeader.id='victory';
+document.getElementById("victory");
+newHeader.innerHTML = "Baroni is the champion"; */
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Baroni is the champion"
+
